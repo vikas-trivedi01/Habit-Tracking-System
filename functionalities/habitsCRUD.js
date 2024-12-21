53be9bf33e1010dbdd4dc2e0700a88067ab1f808
vikas trivedi <vikastrivedi4522@gmail.com>
2024-12-21 22:41:15 +0530

    implement functionality to have daily checkboxes for habits

--- functionalities/habitsCRUD.js
+++ functionalities/habitsCRUD.js
@@ -6,23 +6,29 @@ document.getElementById("habit-form").addEventListener("submit", addHabit);
 function addHabit(e) {
     e.preventDefault();
 
+    const habitName = document.getElementById("name").value.trim();
+    const habitStartDate = document.getElementById("start-date").value;
     const habitDescription = document.getElementById("description").value.trim();
-    const habitDue = document.getElementById("due-date").value;
+    const habitGoalDays = document.getElementById("goal-days").value;
 
-    if (!habitDescription || !habitDue) {
-        alert("Please enter both description and due date.");
+    if (!habitName && !habitStartDate) {
+        alert("Please enter name and starting date of habit.");
         return;
     }
     //push the habit object in habits array
     habits.push({
-        description: habitDescription,
-        due_date: habitDue,
-        isCompleted: false,
-
+        habitName,
+        habitStartDate,
+        habitDescription,
+        habitGoalDays,
+        compltedDays: 0,
+        isCompleted: false
     });
     //empty the fields
+    document.getElementById("name").value = "";
+    document.getElementById("start-date").value = "";
     document.getElementById("description").value = "";
-    document.getElementById("due-date").value = "";
+    document.getElementById("goal-days").value = "";
 
     //display and save tasks to localstorage
     displayHabits();
@@ -32,36 +38,65 @@ function addHabit(e) {
 function displayHabits() {
 
     const habitList = document.getElementById("habit-list");
+    const habitCompletingList = document.getElementById("habit-completing-list");
     habitList.innerHTML = "";
+    habitCompletingList.innerHTML = "";
 
-    if(habits.length === 0){
+    if (habits.length === 0) {
         habitList.innerText = "No Habits to Display";
     }
     habits.forEach((habit, index) => {
         //create necessary elements
         const habit_elem = document.createElement("div");
-        const habit_buttons = document.createElement("div");
-        const habit_info = document.createElement("div");
+        const habit_complete = document.createElement("div");
+        const habit_card = document.createElement("div");
+        const habit_compliting_container = document.createElement("div");
+        // const habit_compliting_container = document.createElement("div");
 
 
         habit_elem.className = habit.isCompleted ? "habit-item completed" : "habit-item";
         habit_elem.innerHTML = `
-            Habit: ${habit.description}<br>
-            Due Date: ${habit.due_date}<br>`;
-
-        habit_buttons.innerHTML = `
-            <button id="delete" class="btn" onclick="deleteHabit(${index})">Delete</button><br>
-            <button id="edit" class="${habit.isCompleted ? 'hide' : 'btn'}" onclick="editHabit(${index})">Edit</button><br>
-            <button id="mark_completed" class="${habit.isCompleted ? 'hide' : 'hov btn'}" onclick="completeHabit(${index})">Mark Complete</button>
+            <div id="habit-info">
+            Habit: ${habit.habitName}<br>
+            Start Date: ${habit.habitStartDate}<br>
+            Description: ${habit.habitDescription}<br>
+            Goal Days: ${habit.habitGoalDays}<br>
+            Completed Days: ${habit.compltedDays}<br>
+            </div>
+            <div id="icons">
+            <span  id="edit" class="${habit.isCompleted ? 'hide' : 'habit-icons'} " onclick="editHabit(${index})"><i class="fa-regular fa-pen-to-square"></i></span>
+            <span class=" ${habit.isCompleted ? 'habit-icons w-full' : 'habit-icons'} habit-icons " id="delete"  onclick="deleteHabit(${index})"><i class="fa-solid fa-trash-can"></i></span>
+            </div>
             `;
 
-        habit_buttons.className = "buttons";
-        habit_info.className = "habit_info";
+        habit_complete.innerHTML = `
+            <button id="mark-completed" class="${habit.isCompleted ? 'hide' : ''} " onclick="completeHabit(${index})">Mark Complete</button>
+            `;
 
-            habit_info.appendChild(habit_elem);
-            habit_info.appendChild(habit_buttons);
-            habitList.appendChild(habit_info);
-        
+        habit_card.className = "habit-card";
+
+        habit_card.appendChild(habit_elem);
+        habit_card.appendChild(habit_complete);
+        habitList.appendChild(habit_card);
+
+        const habitNameElem = document.createElement("p");
+        habitNameElem.textContent = `Habit Name : ${habit.habitName}`;
+        habitNameElem.className = 'habit-title'
+        const checkbox = '<input type="checkbox" class="habit-checkboxes"></input>';        
+
+        for(let index = 0; index < habit.habitGoalDays; index++) {
+            const container = document.createElement("div");
+            container.innerHTML += `<p>Day ${index+1}:</p>${checkbox}`
+            container.className = 'checkbox-container'
+            habit_compliting_container.appendChild(container);//h_c_c is container for content of one habit
+        }
+
+        habit_compliting_container.className = 'habit-checkboxes-container'
+        // habit_compliting_container.appendChild(habit_compliting_days);
+        // habit_compliting_container.appendChild(habit_compliting_chekboxes);
+        habitCompletingList.appendChild(habitNameElem);
+        habitCompletingList.appendChild(habit_compliting_container);
+
 
     });
 }
@@ -117,7 +152,7 @@ const saveHabitsDebounced = debounce(saveHabits, 1000);
 function loadHabits() {
     try {
         const storedHabits = localStorage.getItem("habits");//fetch habits from localstorage
-        habits = storedHabits?JSON.parse(storedHabits):[];
+        habits = storedHabits ? JSON.parse(storedHabits) : [];
         displayHabits();
 
     } catch (error) {
