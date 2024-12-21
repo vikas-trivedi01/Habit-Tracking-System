34433a27053f8bfb8df5e77e60ae466ba0d9ad1f
vikas trivedi <vikastrivedi4522@gmail.com>
2024-12-21 16:27:11 +0530

    minor changes in script to fetch random quote

--- functionalities/randomQuotes.js
+++ functionalities/randomQuotes.js
@@ -4,10 +4,11 @@ const quotesContainer = document.getElementById("random-quote");
 
 (async () => {
     fetch(requestUrl)
-    .then(response => response.json())
-    .then(data => quotesContainer.innerHTML = data[0].h)//h = html having quote and footer
-    .catch(err => quotesContainer.innerText = err.message)
+        .then(response => response.json())
+        .then(data => quotesContainer.innerHTML += data[0].h)//h = html having quote and footer
+        .catch(err => err.message == 'Failed to fetch' ? quotesContainer.innerText = 'No Quotes Available' : err.message)
 }
 )();//IIFE (Immediately Invoked Function Expression)
+
 
 
