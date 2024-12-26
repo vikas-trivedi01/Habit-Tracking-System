9a842ee2d34db1c3a5ce5df31900dc727fa36f64
vikas trivedi <vikastrivedi4522@gmail.com>
2024-12-26 15:32:50 +0530

    implement functionality to create containers for charts

--- functionalities/progress.js
+++ functionalities/progress.js
@@ -1,31 +1,38 @@
-// import  weeklyProgress  from "./habitsCRUD";
 const progressData = JSON.parse(localStorage.getItem('habitProgress')) || {};
-let val24 = [];
-let val25 = [];
-let val26= [];
+const habits = JSON.parse(localStorage.getItem('habits')) || [];
+const habitNames = [];
+// habits.forEach((habit)=>{
+//      habitNames.push(habit.habitName);
+// })
+// console.log(habitNames)
 
-Object.values(progressData).forEach((habit,index)=>{
-    habit.forEach((habitWeek)=>{
-        val24.push("Habit No" + habitWeek.habit);
-        val25.push(habitWeek.completed);
-        val26.push(index+1);
-    })
+const progressContainer = document.querySelector('.progress-section-container');
+
+habits.forEach((habit,habitIndex)=>{
+
+    const habitContainer = document.createElement('div');
+    habitContainer.classList.add('habit-progress-container');
+
+    const habitContainerHeader = document.createElement('div');
+    habitContainerHeader.classList.add('habit-container-header');
+
+    const habitContainerTitle = document.createElement('h4');
+    habitContainerTitle.innerText = `${habit.habitName}`;
+    habitContainerTitle.classList.add('habit-container-title');
+
+    const habitProgress = document.createElement('canvas');
+    const chartIcon = createChartIcon(habitProgress);
+
+    habitProgress.id = `progress-chart-${habitIndex}`;
+    habitProgress.style.width = '100%';
+    habitProgress.style.maxWidth = '700px';
+    
+    habitContainerHeader.appendChild(habitContainerTitle);
+    habitContainerHeader.appendChild(chartIcon);
+
+    habitContainer.appendChild(habitContainerHeader);
+    habitContainer.appendChild(habitProgress);
+    
+    progressContainer.appendChild(habitContainer);
+    
 })
-
- new Chart("stats",{
-    type:"line",
-    data:{
-        labels:val24,
-        datasets:[{
-            borderColor:"orange",
-            data:val25
-        }]
-    },
-    options:{
-        legend:{display:false},
-        scales:{
-            yAxes: [ {ticks:{min:1,max:20}}],
-        }
-    }
-
-});
